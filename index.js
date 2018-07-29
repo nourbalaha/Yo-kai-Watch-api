ca3dcf60ca7b0c2d9b755d9cfdbcac466ab66414
nourbalaha <nourbalaha0@gmail.com>
2018-07-29 21:19:54 +0900

    feat: finished app.get

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
+const config = require("./knexfile");
+
+const bodyParser = require("body-parser");
+
+const knex = require('knex')(config);
+
+app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
+
+
+app.get("/api/", (req, res) => {
+  const query = req.query;
+  knex('yokai_watch').where(query).then((data) => {
+    if (data.length < 1 || data == undefined) {
+      res.send({ "error": "Bad request!", "error_id": 400 });
+    } else {
+      res.json(data);
+    }
+  });
+});
 
 
 
