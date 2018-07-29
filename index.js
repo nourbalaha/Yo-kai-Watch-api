711aad8e62690fe2714882296d48e10ae2defed1
nourbalaha <nourbalaha0@gmail.com>
2018-07-29 22:47:27 +0900

    feat: finished app.put and app.delete

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const knex = require('knex')(config);
 
 app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
 
+app.get("/api/all/", (req, res) => {
+  knex('yokai_watch').then((data) => {
+    if (data.length < 1 || data == undefined) {
+      res.send({ "error": "Bad request!", "error_id": 400 });
+    } else {
+      res.json(data);
+    }
+  });
+});
 
 app.get("/api/", (req, res) => {
   const query = req.query;
@@ -20,11 +29,34 @@ app.get("/api/", (req, res) => {
   });
 });
 
-app.post("/api", (req, res) => {
+app.post("/api/", (req, res) => {
   const body = req.body;
   if(body.name && typeof body.name === "string" && body.name.trim()!== ""){
     knex("yokai_watch").insert(body,"*").then((data)=>{
       res.json(data);
+    });
+  }else{
+    res.send({ "error": "Bad request!", "error_id": 400 , "details": "must provide a valid name"});
+  }
+});
+
+app.put("/api/", (req, res) => {
+  const body = req.body;
+  const query = req.query;
+  if(body.name && typeof body.name === "string" && body.name.trim()!== ""){
+    knex("yokai_watch").where(query).update(req.body).then(()=>{
+      res.json({ "message": "updated!"});
+    });
+  }else{
+    res.send({ "error": "Bad request!", "error_id": 400 , "details": "must provide a valid name"});
+  }
+});
+
+app.delete("/api/", (req, res) => {
+  const query = req.query;
+  if(query){
+    knex("yokai_watch").where(query).del().then(()=>{
+      res.json({ "message": "deleted!"});
     });
   }else{
     res.send({ "error": "Bad request!", "error_id": 400 , "details": "must provide a valid name"});
